2eabe2c9df5e1db6c719fb880d4208350daf3b8f
Joshua <3e2.weechongjingjoshua43@gmail.com>
2022-05-22 01:47:43 +0800

    started on distrbute_reward instruction

--- programs/tradehaus/src/instructions/distribute_rewards.rs
+++ programs/tradehaus/src/instructions/distribute_rewards.rs
@@ -1,68 +1,73 @@
 use anchor_lang::prelude::*;
 
 use crate::state::*;
+use anchor_spl::token::{self, Mint, SetAuthority, TokenAccount, Transfer};
 
 #[derive(Accounts)]
-pub struct DistributeRewards<'info>{
+pub struct DistributeRewards<'info> {
   // define accounts taken in by the distribute_rewards instruction
+  #[account(
+      mut,
+      constraint = player_fund.player == *player.key,
+      constraint = player_fund.game_config == game_config.key(),
+  )]
+  pub player_fund: Account<'info, Fund>,
 
-  //to check which player fund account is in top 10 USD balance amount 
   #[account(mut)]
-  pub player_fund: Account<'info,Fund>,
+  pub player: Signer<'info>,
 
-  // //send the rewards to associated player token wallet from top 10 fund accounts 
-  // #[account(mut)]
-  // pub player_token_wallet: Account<'info, TokenAccount>,
+  #[account(mut)]
+  pub player_token_wallet: Account<'info, TokenAccount>,
 
-  // //not sure how to set the reward escrow owner to game.. but yeah, transfer reward from escrow account to player token wallet 
-  // #[account(mut, has_one = game)]
-  // pub reward_escrow: Account<'info, TokenAccount>,
+  #[account(mut)]
+  pub host: Signer<'info>,
 
-  // //game account which should store all the players account details for checking top N balances and then distributing rewards
-  // pub game_config: Account<'info, Game>, 
+  #[account(mut)]
+  pub reward_mint: Account<'info, Mint>,
+
+  #[account(
+      mut, 
+      seeds = [b"reward-escrow".as_ref(), game_config.to_account_info().key.as_ref()],
+      bump,
+      token::authority = host,
+      token::mint = reward_mint,
+  )]
+  pub reward_escrow: Account<'info, TokenAccount>,
+
+  #[account(
+      mut,
+      constraint = Clock::get().unwrap().unix_timestamp < game_config.end_time.try_into().unwrap(),
+  )]
+  pub game_config: Account<'info, Game>,
+
+  pub token_program: AccountInfo<'info>,
 }
 
-// #[error_code]
-// pub enum ErrorCode {
-//   #[msg("Game is currently opened")]
-//   GameOpened,
-// }
-
-impl <'info> DistributeRewards <'info> {
+impl<'info> DistributeRewards<'info> {
   // // implement required functions for DistributeRewards struct
-  // //grab addresses from vecotr / hashmap from game account 
-   
-  // fn transfer_reward(&self, amount:u32) -> Result<()> {
-    
-  //   let sender = &self.game;
-  //   let sender_tokens = &self.reward_escrow;
-  //   let recipient_tokens = &self.player_token_wallet;
-  //   let token_program = &self.token_program;
+  // //grab addresses from vecotr / hashmap from game account
+  fn transfer_reward(&self, amount:u64) -> Result<()> {
+    let sender = &self.game_config;
+    let sender_tokens = &self.reward_escrow;
+    let recipient_tokens = &self.player_token_wallet;
+    let token_program = &self.token_program;
 
-  //   let context::Transfer {
-  //       from: sender_tokens.to_account_info(),
-  //       to: recipient_tokens.to_account_info(),
-  //       authority: sender.to_account_info(),
-  //   };
-  //   seeds
+    let context = Transfer {
+        from: sender_tokens.to_account_info(),
+        to: recipient_tokens.to_account_info(),
+        authority: sender.to_account_info(),
+    };
 
-  //   token::Transfer(
-  //     CpiContext::new_with_signer(
-  //       token_program.to_account_info(),
-  //       context
-  //     ),
-  //     amount,
-  //   );
-
-
-  
+    token::transfer(
+      CpiContext::new_with_signer(token_program.to_account_info(),context),
+      amount,
+    );
+  }
 }
 
 pub fn handler(ctx: Context<DistributeRewards>, amount: u32) -> Result<()> {
   // // core instruction to allow users to distribute rewards evenly
   // // to top N users as per indicated in game account. current > end_datetime
-  // if current < end_datetime {
-  //   return Err(error!(ErrorCode::GameOpened));
 
   Ok(())
 }
